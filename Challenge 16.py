2e02b63a0f499e9c927a87f577253e4ee738c301
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-08-12 02:05:25 +0100

    Update Challenge 16.py

--- Challenge 16.py
+++ Challenge 16.py
@@ -6,5 +6,45 @@
 # otherwise display the message “Take an umbrella”.
 # If they did not answer yes to the first question, display the answer
 # “Enjoy your day”.
+import sys
+
+yes_list = ['yes', 'y', '1']
+no_list = ['no', 'n']
 
 
+def input_checker(string):
+    while True:
+        try:
+
+            answer = input(string)
+
+            if answer.lower() not in yes_list and answer.lower() \
+                    not in no_list:
+                print('Please enter yes or no!')
+                continue
+            elif answer.lower() in yes_list:
+                return True
+            else:
+                return False
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+
+    question = 'Is it raining? '
+    result = input_checker(question)
+
+    if not result:
+        print('Enjoy your day!')
+        sys.exit()
+
+    question = 'Is it windy? '
+    result = input_checker(question)
+
+    if result:
+        print('It is too windy for an umbrella.')
+        sys.exit()
+
+    print('Take an umbrella')
