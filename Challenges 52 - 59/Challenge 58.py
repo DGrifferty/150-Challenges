e897299b8a11225d8b8b5e5e44b4b5f9d6a19498
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-08-14 03:58:02 +0100

    Update Challenge 58.py

--- Challenges 52 - 59/Challenge 58.py
+++ Challenges 52 - 59/Challenge 58.py
@@ -4,9 +4,11 @@
 # Ask the user to enter the answer. If they get it right add a
 # point to their score. At the end of the quiz, tell them how
 # many they got correct out of five.
-
 import random
 from typing import List, Tuple
+import time
+
+dev = True
 
 
 def get_num_int(prompt: str) -> int:
@@ -66,18 +68,28 @@ def get_difficulty():
             print(e)
 
 
+def get_lower_higher_nums(difficulty: str, symbol: str)\
+        -> Tuple[int, int]:
+    num_dif_dict = {
+        # + -, * /
+        'e': [(1, 15), (1, 5)],
+        'm': [(1, 100), (1, 10)],
+        'h': [(1, 1000), (1, 50)]
+    }
+    if symbol == '+' or symbol == '-':
+        return num_dif_dict[difficulty][0]
+    else:
+        return num_dif_dict[difficulty][1]
+
+
 def get_equation(difficulty: str = 'medium') -> Tuple[str, float]:
-    if difficulty == 'e':
-        lowest_num, highest_num = 1, 10
-    elif difficulty == 'm':
-        lowest_num, highest_num = 1, 50
-    elif difficulty == 'h':
-        lowest_num, highest_num = 1, 100
+
+    symbol = get_symbol()
+
+    lowest_num, highest_num = get_lower_higher_nums(difficulty, symbol)
 
     num1 = random.randint(lowest_num, highest_num)
     num2 = random.randint(lowest_num, highest_num)
-
-    symbol = get_symbol()
 
     if symbol == '+':
         answer = num1 + num2
@@ -104,14 +116,23 @@ if __name__ == '__main__':
 
         question, answer = get_equation(difficulty)
 
+        if dev:
+            print(answer)
+
+        start_time = time.time()
+
         user_answer = get_num_float(question)
 
-        if user_answer == answer:
+        time_took = time.time() - start_time
+
+        if user_answer == round(answer, 2):
             print('correct!')
+            print(f'You took - {time_took}s')
             correct_count += 1
             continue
         else:
             print('Incorrect')
+            print(f'You took - {time_took}s')
             # while user_answer != answer:
             #     print('incorrect try again!')
             #     user_answer = get_num(question)
@@ -121,10 +142,14 @@ if __name__ == '__main__':
           '\n'
           f'That is {(correct_count / num_questions) * 100}%!')
 
-# todo: change lower number and higher number depending on difficulty
-# and symbol
+
+# todo: sort out issues with float answers - user inputs 1.33 not correct
+# todo: as comp answer stores it as 1.33
 # Todo: Create timer to tell user how long it took
-# todo: time each question
+# todo: time each question + average at the end
 # todo: create mode where user rushes against timer
 # todo: add square, square root functionality etc.
 # todo: create gui
+# todo: create file to store users times and difficulty date they completed
+# todo: questions and add this to section on gui for them to see
+# todo: improvment
