c2a5dbabb0de24926a734b0c3b86bb1b1e40f516
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-08-18 00:20:49 +0100

    Update Challenge 93.py

--- Challenges 88 - 95/Challenge 93.py
+++ Challenges 88 - 95/Challenge 93.py
@@ -4,9 +4,7 @@
 # Remove it from the original array and save it in a new array.
 
 import array as ar
-import random
 import numpy as np
-from typing import List
 
 
 def get_num_int(prompt: str) -> int:
@@ -20,6 +18,50 @@ def get_num_int(prompt: str) -> int:
         except Exception as e:
             print(e)
 
+
 if __name__ == '__main__':
 
-    
+    # Using built in array module
+
+    user_ar = ar.array('i')
+
+    for i in range(5):
+        user_ar.append(get_num_int(f'Enter number {i + 1}: '))
+
+    user_ar = sorted(user_ar)
+    print(user_ar)
+
+    while True:
+
+        num = get_num_int('Please select a number to remove - ')
+
+        if num in user_ar:
+            user_removed = ar.array('i', [num])
+            user_ar.remove(num)
+            break
+
+    print(user_ar)
+    print(user_removed)
+
+    # Using numpy module
+
+    user_np = np.array([], dtype=np.int32)
+
+    for i in range(5):
+        user_np = np.append(user_np,
+                            get_num_int(f'Enter number {i + 1}: '))
+
+    user_np = np.sort(user_np)
+    print(user_np)
+
+    while True:
+
+        num = get_num_int('Please select a number to remove - ')
+
+        if num in user_np:
+            user_removed_np = np.array(num, dtype=np.int32)
+            user_np = np.delete(user_np, np.argwhere(user_np == num))
+            break
+
+    print(user_np)
+    print(user_removed_np)
