168b3733ceee31eb27a51e1765ef149a86e37254
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-08-22 03:04:01 +0100

    Update Challenge 116.py

--- Challenges 111 - 117/Challenge 116.py
+++ Challenges 111 - 117/Challenge 116.py
@@ -28,11 +28,48 @@ def print_csv_table(lst: List, print_row_num: bool = False,
 	return string
 
 
+def get_num_int(prompt: str) -> int:
+    """Function to check if users input is an integer"""
+
+    while True:
+        try:
+            number = int(input(prompt))
+
+            return number
+
+        except Exception as e:
+            print(e)
+
+
 if __name__ == '__main__':
+
 
 	with open('Books.csv', 'r+') as f:
 		table = f.readlines()
-		print_csv_table(table, print_row_num=True)
 
+	print_csv_table(table, print_row_num=True)
 
+	while True:  # Deleting row
 
+		row_to_delete = get_num_int('Enter row to delete: ')
+		if 1 <= row_to_delete <= len(table):
+			del table[row_to_delete - 1]
+			print_csv_table(table, print_row_num=True)
+			break
+		else:
+			print(f'Please enter a number between 1 and {len(table)}')
+
+	while True:  # Changing row
+
+		row_to_change = get_num_int('Enter row to change: ')
+
+		if 1 <= row_to_change <= len(table):
+			table[row_to_change - 1] = input('Enter new record: ') + '\n'
+			print_csv_table(table, print_row_num=True)
+			break
+		else:
+			print(f'Please enter a number between 1 and {len(table)}')
+
+	with open('Books.csv', 'w') as f:
+		for element in table:
+			f.write(element)
