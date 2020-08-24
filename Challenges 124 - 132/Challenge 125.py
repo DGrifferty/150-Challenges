a37edd2796527246ded5ac07e14091192ce7a2aa
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-08-24 17:47:01 +0100

    Update Challenge 125.py

---
+++ Challenges 124 - 132/Challenge 125.py
@@ -0,0 +1,11 @@
+# 125
+# Write a program that can be used instead of rolling a six-sided
+# die in a board game. When the user clicks a button it should
+# display a random whole number between 1 to 6 (inclusive).
+
+import tkinter as tk
+import random
+
+window = tk.Tk()
+
+
