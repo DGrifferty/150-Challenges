0cdcba48b5d5e5c8434ca296a5ed946596e617fe
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-08-24 04:38:01 +0100

    Create Challenges 124.py

---
+++ Challenges 124 - 132/Challenges 124.py
@@ -0,0 +1,33 @@
+# 124
+# Create a window that will ask the user to enter their name.
+# When they click on a button it should display the message
+# “Hello” and their name and change the background colour
+# and font colour of the message box.
+
+# (Using Tkinter)
+
+import tkinter as tk
+import random
+window = tk.Tk()
+
+
+def _random_rgb():
+    rgb = []
+    for i in range(3):
+        rgb.append(random.randint(0, 255))
+    rgb = tuple(rgb)
+
+    return "#%02x%02x%02x" % rgb
+
+
+def button1_command():
+    print('Hello')
+
+
+window.title('Title')
+window.geometry('400x400')
+button1x, button1y = 30, 20
+button1 = tk.Button(text='Click here', command=button1_command)
+button1.place(x=button1x, y=button1y, width=120, height=25)
+
+window.mainloop()
