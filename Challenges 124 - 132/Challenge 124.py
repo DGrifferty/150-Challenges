a819c665a4cd448517a307402b241398ea12253d
DGrifferty <69149609+DGrifferty@users.noreply.github.com>
2020-08-24 17:45:31 +0100

    Update Challenge 124.py

--- Challenges 124 - 132/Challenge 124.py
+++ Challenges 124 - 132/Challenge 124.py
@@ -23,17 +23,26 @@ def _random_rgb():
     return "#%02x%02x%02x" % rgb
 
 
-def button1_command(x, y):
-    msg = tk.Label(window, text = 'Hello')
-    msg.place(x=x, y=y + 30)
+def _button1_command(x, y):
+    name = textbox1.get()
+    msg = tk.Label(window, text=f'Hello {name}')
+    msg.place(x=x, y=y + 40)
     button1['bg'] = _random_rgb()
     button1['fg'] = _random_rgb()
 
 
 window.title('Title')
 window.geometry('400x400')
-button1x, button1y = 30, 20
-button1 = tk.Button(text='Click here', command=partial(button1_command, button1x, button1y))
+
+button1x, button1y = 240, 50
+button1 = tk.Button(text='Click here',
+                    command=partial(_button1_command, button1x-100,
+                                    button1y))
 button1.place(x=button1x, y=button1y, width=120, height=25)
+
+textbox1 = tk.Entry(text='')
+textbox1.place(x=button1x - 200, y=button1y, width=200, height=25)
+textbox1['justify'] = 'center'
+textbox1.focus()
 
 window.mainloop()
